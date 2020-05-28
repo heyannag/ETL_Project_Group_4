767091e356c0806aea512497f515d39e77e0664e
Lisa Reed Preston <lisareedpreston@Lisas-MacBook-Pro.local>
2020-05-28 12:42:55 -0500

    making progress with the apple api

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    description="Apple Music Python Client",
+    install_requires=['cryptography', 'PyJWT', 'requests'],
+    keywords="apple music api wrapper",
+    license="MIT",
+    name="apple-py-music",
+    packages=["applepymusic"],
+    # TODO:
+    # test_suite="tests",
+    url='https://github.com/rcrdclub/apple-py-music',
+    version='0.6.0',
+)
